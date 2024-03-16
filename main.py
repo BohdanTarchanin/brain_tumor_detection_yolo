6ea939c191afc894bd3a6f595b9c8e8bf628b184
BohdanTarchanin <32395057+BohdanTarchanin@users.noreply.github.com>
2024-03-16 17:06:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from PIL import Image
 
 # Set title and description
-st.title("Brain Tumor Detection App")
-st.write("This app allows you to upload an MRI image for brain tumor detection.")
+st.title("Розпізнавання і виявлення патологічних утворень головного мозку")
+st.write("Ця програма дозволяє завантажувати зображення МРТ для виявлення пухлини мозку")
 
 # Upload MRI image
-uploaded_file = st.file_uploader("Upload MRI image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Завантажте зображення МРТ", type=["jpg", "jpeg", "png"])
